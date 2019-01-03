6116e98e719832e282588bb758a16d23bc7ad8d6
Lucas Mongrain <xeaprog@gmail.com>
2019-01-02 21:31:38 -0500

    cannot move when the text edit is focus

--- Script/buildingCamera.gd
+++ Script/buildingCamera.gd
@@ -3,23 +3,24 @@ extends Camera2D
 const camera_margin = 20
 const mouvementSpeed = 7
 var mouvement = Vector2(0,0)
+onready var ctrl_global = get_node("/root/ctrl_global")
 
 
 func _physics_process(delta):
 	if(self.current):
 		mouvement = Vector2(0,0)
 		
-		#right
-		if(Input.is_action_pressed("CAMERA_RIGHT")):
-			mouvement.x += 1
-		#left
-		if(Input.is_action_pressed("CAMERA_LEFT")):
-			mouvement.x -= 1
-		#up
-		if(Input.is_action_pressed("CAMERA_UP")):
-			mouvement.y -= 1
-		#down
-		if(Input.is_action_pressed("CAMERA_DOWN")):
-			mouvement.y += 1
+		if(!self.ctrl_global.bool_text_select):
+			if(Input.is_action_pressed("CAMERA_RIGHT")):
+				mouvement.x += 1
+			#left
+			if(Input.is_action_pressed("CAMERA_LEFT")):
+				mouvement.x -= 1
+			#up
+			if(Input.is_action_pressed("CAMERA_UP")):
+				mouvement.y -= 1
+			#down
+			if(Input.is_action_pressed("CAMERA_DOWN")):
+				mouvement.y += 1
 		
 		self.position += (mouvement.normalized() * mouvementSpeed)
