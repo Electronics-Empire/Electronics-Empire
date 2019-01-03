2ee7439edd14f7c377ea0eca687cb50442ae5651
Lucas Mongrain <xeaprog@gmail.com>
2019-01-02 22:01:56 -0500

    add a panel

--- Script/PanelGUI.gd
+++ Script/PanelGUI.gd
@@ -1,9 +1,5 @@
 extends Panel
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
