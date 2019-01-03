100ebcc417ebdbf0aba2aaf5c91794057ed262aa
Lucas Mongrain <xeaprog@gmail.com>
2019-01-03 15:36:15 -0500

    solve focus problem

--- Script/PanelGUI.gd
+++ Script/PanelGUI.gd
@@ -1,5 +1,7 @@
 extends Panel
 
+onready var textEdit = get_node("TextEdit")
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
@@ -9,3 +11,9 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func gui_input(event):
+	if event is InputEventMouseButton:
+		textEdit.release_focus()
+	pass
