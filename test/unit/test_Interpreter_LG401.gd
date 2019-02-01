d94a78d0c78cb0716724df0c6faff5b7eedbc37c
Lucas Mongrain <xeaprog@gmail.com>
2019-01-31 20:18:38 -0500

    finish LG401 interpreter unit test

--- test/unit/test_Interpreter_LG401.gd
+++ test/unit/test_Interpreter_LG401.gd
@@ -1,7 +1,11 @@
 extends "res://addons/gut/test.gd"
 
 var interpreter = load("res://Script/Interpreter_LG401.gd").new()
-var operand1 = 0
+var operand1
+var operand2
+
+const direction_string = ["north", "south", "east", "west"]
+const random_string = ["string1", "string2", "string3", "string4"]
 
 func before_each():
 	watch_signals(interpreter)
@@ -28,3 +32,26 @@ func test_attack():
 	interpreter.evaluate()
 	
 	assert_signal_emitted(interpreter, "attack_signal")
+
+func test_mine():
+	interpreter.load_line("mine")
+	interpreter.evaluate()
+	
+	assert_signal_emitted(interpreter, "mine_signal")
+
+func test_build():
+	operand1 = random_string[randi()%random_string.size()]
+	operand2 = random_string[randi()%random_string.size()]
+	
+	interpreter.load_line("bld \"" + operand1 + "\" \"" + operand2 + "\"")
+	interpreter.evaluate()
+	
+	assert_signal_emitted_with_parameters(interpreter, "build_signal", [operand1, operand2])
+
+func test_rot():
+	operand1 = direction_string[randi()%direction_string.size()]
+	
+	interpreter.load_line("rot " + operand1)
+	interpreter.evaluate()
+	
+	assert_signal_emitted_with_parameters(interpreter, "rotate_signal", [operand1])
